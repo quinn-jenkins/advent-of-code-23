47c1ccab725a1b815b746d9a9ea323b862c962e7
Quinn Jenkins <qjenkins@ctic.us>
2023-12-14 11:11:32 -0800

    Day 14 implementation

---
+++ day14/day14.py
@@ -0,0 +1,110 @@
+import time
+import functools
+
+def main(filename: str, partTwo : bool):
+    with open(filename) as file:
+        lines = [line.rstrip() for line in file]
+        # print('\n'.join(' '.join(str(x) for x in row) for row in lines))
+        # rotate one extra time first so that "Left" is "North" because for some reason that makes more sense in my head
+        lines = rotateCCW(lines)
+        
+        if partTwo:
+            print("Running part 2")
+            numCycles = 1000000000
+            knownStates = {}
+
+            for i in range(numCycles):
+                lines = runOneCycle(tuple(lines))
+                state = hash(lines)
+                # print(f"Cycle # {i}")
+                # print('\n'.join(' '.join(str(x) for x in row) for row in lines))
+                # print(20*"-")
+                if state in knownStates:
+                    # we found a repetition, so now we can extrapolate out and not run a lot of the remaining cycles
+                    cycleStart = knownStates[state]
+                    cycleSize = i - cycleStart
+                    print(f"Found a repeated state after {i} cycles! Cycle is {cycleSize} long and starts at iteration {cycleStart}.")
+                    break
+                else:
+                    knownStates[state] = i
+            # find how far into a cycle we are after the last full cycle before the end
+            numCyclesLeft = (numCycles - (cycleStart + 1)) % cycleSize
+            print(f"Running {numCyclesLeft} times to get to the correct end state.")
+            for i in range(numCyclesLeft):
+                lines = runOneCycle(lines)
+        else:
+            print("Running part 1")
+            lines = tilt(lines)
+        
+        #undo our extra rotation
+        lines = rotateClockwise(lines)
+
+        # print('\n'.join(' '.join(str(x) for x in row) for row in lines))
+        load = countLoad(lines)
+        
+        print(f"{'Part Two' if partTwo else 'Part One'} : {load}")
+
+@functools.cache
+def runOneCycle(lines):
+    # rotate north
+    lines = tilt(lines)
+    lines = rotateClockwise(lines)
+    
+    # rotate west
+    lines = tilt(lines)
+    lines = rotateClockwise(lines)
+
+    # rotate south
+    lines = tilt(lines)
+    lines = rotateClockwise(lines)
+
+    # rotate east
+    lines = tilt(lines)
+    lines = rotateClockwise(lines)
+
+    return lines
+
+def rotateCCW(lines):
+    return tuple(zip(*lines))[::-1]
+
+def rotateClockwise(lines):
+    return tuple(zip(*lines[::-1]))
+
+def tilt(lines):
+    tilted = []
+    for line in lines:
+        tilted.append(tiltLine(line))
+    return tilted
+
+@functools.cache
+def tiltLine(line):
+    blockedIndex = 0
+    line = list(line)
+    for i, ch in enumerate(line):
+        if i > blockedIndex:
+            if ch == "#":
+                blockedIndex = i+1
+            elif ch == "O":
+                # move it to the blocked index, and open up its space
+                line[blockedIndex] = "O"
+                line[i] = "."
+                blockedIndex += 1
+        else:
+            if ch in "O#":
+                blockedIndex += 1
+    return tuple(line)
+
+def countLoad(lines) -> int:
+    totalLoad = 0
+    load = len(lines)
+    for line in lines:
+        totalLoad += load * line.count("O")
+        load -= 1
+    return totalLoad
+
+if __name__ == "__main__":
+    filename = "day14/input.txt"
+    main(filename, False)
+    startTime = time.time()
+    main(filename, True)
+    print(f"Total time to run {time.time() - startTime}")
